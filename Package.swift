ca1e3df3b6a6166719e23d80fae75ec968a4c2a6
manohara347 <manoharakle@gmail.com>
2023-11-02 11:44:53 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 //let url = "https://cdn.appdynamics.com/eum-mobile/iOSAgent-2023.8.0.4197.zip"
 //let checksum = "520fb6a6edafb331f7dd3c72c9b337e7ee84d63a9d396d924135c6e98b340f7a"
-let url = "https://github.com/manohara347/AppDynamicsAgent/releases/download/1.0.16/ADEUMInstrumentation.xcframework.zip"
-let checksum = "2418cf72ee407e0ab2df676a86958354d97e22a5174a1e49a61bed13d6ef3d1f"
+let url = "https://appdynamics.jfrog.io/artifactory/maven-releases/com/appdynamics/eum/iOSAgentSPM/2023.10.1.4260/iOSAgentSPM-2023.10.1.4260.zip"
+let checksum = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 //let url = "https://github.com/manohara347/AppDynamicsAgent/releases/download/1.0.18/mobile-ios-2023.7.0.zip"
 //let checksum = "7cfcad433bff4f07026d94fe31e83ab5fbc446cbd71b3b476ec509d2e7fe3e15"
 
